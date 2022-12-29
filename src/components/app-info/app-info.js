f3c81d4e4174ce07532701afa6e2889c4685e5c2
Maria-tanina <mariyatkach2002@gmail.com>
2022-12-29 14:04:24 +0200

    Application framework

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting at Entypon</h1>
+            <h2>Total number of employees: </h2>
+            <h2>The award will be given to: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
